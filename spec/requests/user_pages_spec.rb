918ae1c23495e055eefad7f8d56f295b265cb210
Angela Francis <angela.marie.francis@gmail.com>
2013-05-08 12:52:36 -0500

    Finished lab 8.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -49,7 +49,14 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
-  end # end "describe signup"
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end # end "with valid information"
+  end # end "describe signup"
 end # end "describe user pages"
